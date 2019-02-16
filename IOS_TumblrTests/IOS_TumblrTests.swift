4481df423490becd7a3baebe5b63212351ac81da
Sophia <qyoung@ucsd.edu>
2019-02-15 19:22:15 -0800

    Initial Commit

---
+++ IOS_TumblrTests/IOS_TumblrTests.swift
@@ -0,0 +1,34 @@
+//
+//  IOS_TumblrTests.swift
+//  IOS_TumblrTests
+//
+//  Created by Sophia Sun on 2/15/19.
+//  Copyright © 2019 Sophia Qi Young. All rights reserved.
+//
+
+import XCTest
+@testable import IOS_Tumblr
+
+class IOS_TumblrTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
