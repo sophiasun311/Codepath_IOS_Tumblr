31392b901c11150e3f05ddf5ec0dcf5bddb401ee
Sophia <qyoung@ucsd.edu>
2019-02-19 18:57:18 -0800

    display posts for blog Humans of New York

---
+++ IOS_Tumblr/PhotoCell.swift
@@ -0,0 +1,25 @@
+//
+//  PhotoCell.swift
+//  IOS_Tumblr
+//
+//  Created by Sophia Sun on 2/19/19.
+//  Copyright © 2019 Sophia Qi Young. All rights reserved.
+//
+
+import UIKit
+
+class PhotoCell: UITableViewCell {
+
+    @IBOutlet weak var photoImageView: UIImageView!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
